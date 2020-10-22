7b7a6270bc5abceb6e402bc8909526153d07bd2a
tyfirth <tyfirth@gmail.com>
2020-10-21 21:07:54 -0700

    add session controls to application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,12 +6,47 @@ class ApplicationController < Sinatra::Base
 
   enable :sessions
   set :session_secret, "IYAMAwesome123!"
-  
+
   end
 
   get "/" do
     erb :index
   end
 
+  get '/registrations/signup' do
+    erb :'/registrations/signup'
+  end
+
+  post '/registrations' do
+    @user = User.new(
+      name: params["name"],
+      email: params["email"],
+      password: params["password"]
+    )
+    @user.save
+    session[:user_id] = @user.id
+
+    redirect '/users/home'
+  end
+
+  get '/sessions/login' do
+    erb :'/sessions/login'
+  end
+
+  post '/sessions' do
+    @user = User.find_by(name: params[:name], password: params[:password])
+
+    if @user
+      session[:user_id] = @user.id
+      redirect 'users/home'
+    end
+
+    redirect '/sessions/login'
+  end
+
+  get '/users/home' do
+    @user = User.find(session[:user_id])
+    erb :'/users/home'
+  end
 
 end
